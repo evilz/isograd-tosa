a58f875dd417d416a6bd50beeb1608efb4416f7e
Vincent B <evilznet@gmail.com>
2016-12-12 15:10:42 +0100

    Update Program.cs

--- SocieteGeneralOctober2016/2.b/Program.cs
+++ SocieteGeneralOctober2016/2.b/Program.cs
@@ -8,11 +8,12 @@ namespace SocieteGeneralOctober2016._2.b
         static void Main(string[] args)
         {
             var input = Console.In;
-            //var input = new StringReader("string");
+            //var input = new StringReader("10\n34\n4 3\n2 3\n3 1\n2 2\n4 3\n4 4\n3 1\n1 2\n3 1\n2 3\n1 2\n4 1\n2 3\n1 4\n1 3\n4 1\n1 2\n4 4\n2 1\n1 1\n3 1\n1 2\n2 1\n4 1\n2 1\n3 2\n2 3\n3 4\n1 4\n4 2\n2 3\n2 2\n1 1\n2 3");
 
-            var count = int.Parse(input.ReadLine());
+            var s = int.Parse(input.ReadLine());
+            var n = int.Parse(input.ReadLine());
 
-            var many = Enumerable.Range(0, count)
+            var steps = Enumerable.Range(0, n)
                 .Select(_ =>
                     input.ReadLine()
                         .Split(' ')
@@ -20,9 +21,16 @@ namespace SocieteGeneralOctober2016._2.b
                         .ToArray())
                         .ToArray();
 
-            var output = "";
-
-            Console.WriteLine(output);
+            var A = 0;
+            var B = 0;
+            for (int i = 0; i < steps.Length; i++)
+            {
+                A += steps[i][0];
+                B += steps[i][1];
+                if(A >= s && B >= s) Console.WriteLine("NO WINNER");
+                if(A >= s ) Console.WriteLine("A");
+                if(B >= s) Console.WriteLine("B");
+            }
 
         }
     }
