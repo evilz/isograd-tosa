9654b8df285c7bb117f91775ecfe505724336478
Vincent B <evilznet@gmail.com>
2016-12-12 15:11:55 +0100

    Update Program.cs

--- SocieteGeneralOctober2016/5.e/Program.cs
+++ SocieteGeneralOctober2016/5.e/Program.cs
@@ -1,14 +1,41 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace SocieteGeneralOctober2016._5.e
 {
+
+
     public class Program
     {
+        private class key
+        {
+            
+            public key(string value)
+            {
+                Value = value;
+                Children = new List<key>();
+            }
+
+            public int Level
+            {
+                get
+                {
+                    if(Children.Any())
+                    { return Children.Select(key1 => key1.Level).Max() + 1;}
+                    return 1;
+                }
+            }
+
+            public string Value { get; set; }
+            public List<key> Children { get; private set; } 
+        }
+       
         static void Main(string[] args)
         {
             var input = Console.In;
-            //var input = new StringReader("string");
+            //var input = new StringReader("6\nhome office\nhome garage\nhome parents_home\noffice lab\noffice library\nlibrary main_entrance");
 
             var count = int.Parse(input.ReadLine());
 
@@ -16,13 +43,30 @@ namespace SocieteGeneralOctober2016._5.e
                 .Select(_ =>
                     input.ReadLine()
                         .Split(' ')
-                        .Select(int.Parse)
+                        
                         .ToArray())
                         .ToArray();
 
-            var output = "";
+            var dic = new Dictionary<string,key>();
 
-            Console.WriteLine(output);
+            foreach (var key in many)
+            {
+                dic[key[0]] = new key(key[0]);
+                dic[key[1]] = new key(key[1]);
+            }
+
+            foreach (var key in many)
+            {
+                var master = dic[key[0]];
+                var slave = dic[key[1]];
+
+
+                master.Children.Add(slave);
+            }
+
+
+            var master2 = dic.Values.OrderByDescending(key => key.Level).First();
+            Console.WriteLine(master2.Level);
 
         }
     }
